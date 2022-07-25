507d6024c6e747408b4385524b1aa72e3d96b1c6
nikerocker <nekhileshk@gmail.com>
2022-07-26 02:51:24 +0530

    updated

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Banner>This is a banner!</Banner>
+  </div>
+</template>
+
+<script>
+import Banner from './components/Banner.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
