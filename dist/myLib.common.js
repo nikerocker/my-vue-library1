cefb154731fe799747753ac1d78511a109e38ec2
nikerocker <nekhileshk@gmail.com>
2022-07-26 16:55:50 +0530

    udpated

---
+++ dist/myLib.common.js
@@ -0,0 +1,366 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	!function() {
+/******/ 		__webpack_require__.p = "";
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ entry_lib; }
+});
+
+;// CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/setPublicPath.js
+/* eslint-disable no-var */
+// This file is imported into lib/wc client bundles.
+
+if (typeof window !== 'undefined') {
+  var currentScript = window.document.currentScript
+  if (false) { var getCurrentScript; }
+
+  var src = currentScript && currentScript.src.match(/(.+\/)[^/]+\.js(\?.*)?$/)
+  if (src) {
+    __webpack_require__.p = src[1] // eslint-disable-line
+  }
+}
+
+// Indicate to webpack that this file can be concatenated
+/* harmony default export */ var setPublicPath = (null);
+
+;// CONCATENATED MODULE: external {"commonjs":"vue","commonjs2":"vue","root":"Vue"}
+var external_commonjs_vue_commonjs2_vue_root_Vue_namespaceObject = require("vue");
+var external_commonjs_vue_commonjs2_vue_root_Vue_default = /*#__PURE__*/__webpack_require__.n(external_commonjs_vue_commonjs2_vue_root_Vue_namespaceObject);
+;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/BannerComponent.vue?vue&type=template&id=a46fbfaa&scoped=true&
+var render = function render() {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c('div', {
+    staticClass: "banner",
+    class: `banner__${_vm.position}`,
+    style: _vm.bannerStyles
+  }, [_vm._t("default")], 2);
+};
+
+var staticRenderFns = [];
+
+;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/BannerComponent.vue?vue&type=script&lang=js&
+const defaultStyles = {
+  left: 0,
+  right: 0 // margin: auto,
+
+};
+/* harmony default export */ var BannerComponentvue_type_script_lang_js_ = ({
+  name: 'BannerComponent',
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+
+      validator(position) {
+        return ['top', 'bottom'].indexOf(position) > -1;
+      }
+
+    },
+    styles: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data() {
+    return {
+      bannerStyles: { ...defaultStyles,
+        ...this.styles
+      }
+    };
+  }
+
+});
+;// CONCATENATED MODULE: ./src/components/BannerComponent.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_BannerComponentvue_type_script_lang_js_ = (BannerComponentvue_type_script_lang_js_); 
+;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-12.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-12.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-12.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/BannerComponent.vue?vue&type=style&index=0&id=a46fbfaa&prod&scoped=true&lang=css&
+// extracted by mini-css-extract-plugin
+
+;// CONCATENATED MODULE: ./src/components/BannerComponent.vue?vue&type=style&index=0&id=a46fbfaa&prod&scoped=true&lang=css&
+
+;// CONCATENATED MODULE: ./node_modules/@vue/vue-loader-v15/lib/runtime/componentNormalizer.js
+/* globals __VUE_SSR_CONTEXT__ */
+
+// IMPORTANT: Do NOT use ES2015 features in this file (except for modules).
+// This module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle.
+
+function normalizeComponent(
+  scriptExports,
+  render,
+  staticRenderFns,
+  functionalTemplate,
+  injectStyles,
+  scopeId,
+  moduleIdentifier /* server only */,
+  shadowMode /* vue-cli only */
+) {
+  // Vue.extend constructor export interop
+  var options =
+    typeof scriptExports === 'function' ? scriptExports.options : scriptExports
+
+  // render functions
+  if (render) {
+    options.render = render
+    options.staticRenderFns = staticRenderFns
+    options._compiled = true
+  }
+
+  // functional template
+  if (functionalTemplate) {
+    options.functional = true
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = 'data-v-' + scopeId
+  }
+
+  var hook
+  if (moduleIdentifier) {
+    // server build
+    hook = function (context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+      // 2.2 with runInNewContext: true
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__
+      }
+      // inject component styles
+      if (injectStyles) {
+        injectStyles.call(this, context)
+      }
+      // register component module identifier for async chunk inferrence
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier)
+      }
+    }
+    // used by ssr in case component is cached and beforeCreate
+    // never gets called
+    options._ssrRegister = hook
+  } else if (injectStyles) {
+    hook = shadowMode
+      ? function () {
+          injectStyles.call(
+            this,
+            (options.functional ? this.parent : this).$root.$options.shadowRoot
+          )
+        }
+      : injectStyles
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // for template-only hot-reload because in that case the render fn doesn't
+      // go through the normalizer
+      options._injectStyles = hook
+      // register for functional component in vue file
+      var originalRender = options.render
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context)
+        return originalRender(h, context)
+      }
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook]
+    }
+  }
+
+  return {
+    exports: scriptExports,
+    options: options
+  }
+}
+
+;// CONCATENATED MODULE: ./src/components/BannerComponent.vue
+
+
+
+;
+
+
+/* normalize component */
+
+var component = normalizeComponent(
+  components_BannerComponentvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "a46fbfaa",
+  null
+  
+)
+
+/* harmony default export */ var BannerComponent = (component.exports);
+;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/components/Table/DataTable.vue?vue&type=template&id=0956ddc0&
+var DataTablevue_type_template_id_0956ddc0_render = function render() {
+  var _vm = this,
+      _c = _vm._self._c,
+      _setup = _vm._self._setupProxy;
+
+  return _c('div', {
+    staticClass: "container table-responsive py-5"
+  }, [_c('table', {
+    staticClass: "table table-bordered table-hover"
+  }, [_c('thead', {
+    staticClass: "thead-dark"
+  }, [_c('tr', _vm._l(_vm.cols, function (column, i) {
+    return _c('th', {
+      key: i
+    }, [_c('div', [_c('span', [_vm._v(_vm._s(column.label))])])]);
+  }), 0)]), _c('tbody', [_vm.isEmpty ? _c('tr', [_vm._v(" " + _vm._s(_vm.isEmpty) + " "), _c('td', {
+    staticClass: "empty-table-cell"
+  }, [_vm._v(" Nothing Here........ ")])]) : _vm._e(), _vm._l(_vm.rows, function (row, i) {
+    return _c('tr', {
+      key: i
+    }, _vm._l(_vm.cols, function (column, j) {
+      return _c('td', {
+        key: j
+      }, [_c('span', [_vm._v(_vm._s(row[column.key]))])]);
+    }), 0);
+  })], 2)])]);
+};
+
+var DataTablevue_type_template_id_0956ddc0_staticRenderFns = [];
+
+;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./src/components/Table/DataTable.js?vue&type=script&lang=js&
+/* harmony default export */ var DataTablevue_type_script_lang_js_ = ({
+  name: "DataTableTable",
+  props: {
+    rows: Array,
+    cols: Array
+  },
+
+  data() {
+    return {//   rows: this.rows,
+      //   columns: this.columns,
+    };
+  },
+
+  computed: {
+    numberOfColumns() {
+      return this.rows.length;
+    },
+
+    isEmpty() {
+      return this.numberOfColumns.length === 0 ? true : false;
+    }
+
+  },
+
+  mounted() {
+    this.getData();
+  },
+
+  methods: {
+    getData() {
+      console.log("both", this.rows, this.cols, 'isEmpty', this.isEmpty, 'numberOfColumns', this.numberOfColumns);
+    }
+
+  }
+});
+;// CONCATENATED MODULE: ./src/components/Table/DataTable.js?vue&type=script&lang=js&
+ /* harmony default export */ var Table_DataTablevue_type_script_lang_js_ = (DataTablevue_type_script_lang_js_); 
+;// CONCATENATED MODULE: ./src/components/Table/DataTable.vue
+
+
+
+;
+
+
+/* normalize component */
+
+var DataTable_component = normalizeComponent(
+  Table_DataTablevue_type_script_lang_js_,
+  DataTablevue_type_template_id_0956ddc0_render,
+  DataTablevue_type_template_id_0956ddc0_staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* harmony default export */ var DataTable = (DataTable_component.exports);
+;// CONCATENATED MODULE: ./src/components/index.js
+
+
+
+const Components = {
+  BannerComponent: BannerComponent,
+  DataTable: DataTable
+};
+Object.keys(Components).forEach(name => {
+  external_commonjs_vue_commonjs2_vue_root_Vue_default().component(name, Components[name]);
+});
+/* harmony default export */ var components = (Components);
+;// CONCATENATED MODULE: ./node_modules/@vue/cli-service/lib/commands/build/entry-lib.js
+
+
+/* harmony default export */ var entry_lib = (components);
+
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=myLib.common.js.map
